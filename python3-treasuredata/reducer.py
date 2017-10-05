86bd839e470ecbfeef8bb91e8a4a80cc19173c44
gimpei <gim.kobayashi@gmail.com>
2017-10-05 20:12:18 +0900

    up

--- python3-treasuredata/reducer.py
+++ python3-treasuredata/reducer.py
@@ -4,17 +4,17 @@ import re
 import json
 
 def main(argv):
-  key_vals = {}
-
+  key_vals = {} 
   for line in sys.stdin:
     line = line.strip()
     key, val = line.split('\t')
     val = json.loads(val)
-    if key_vals.get(key) is None:
+    if key_vals.get( key ) is None:
       key_vals[key] = {}
-    for timestamp, array in val.items():
-      key_vals[key][timestamp] = array
+    
+    for timestamp, meta in val.items():  
+      key_vals[key][timestamp] = meta
   for key, vals in key_vals.items():
-    print(key + '\t' + json.dumps(vals) )
+    print(key + '\t' + json.dumps(vals, ensure_ascii=False) )
 if __name__ == "__main__":
     main(sys.argv)
