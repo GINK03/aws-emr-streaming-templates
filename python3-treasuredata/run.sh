58e25dc02402ffd1bb647071ee2bf75e9b55bf11
Your Name <gim.kobayashi@gmail.com>
2017-11-17 12:22:02 +0900

    a

--- python3-treasuredata/run.sh
+++ python3-treasuredata/run.sh
@@ -1,1 +1,1 @@
-aws emr add-steps --cluster-id j-1T0RSZ0JH4W85 --steps file://./treasuredata.json --region ap-northeast-1
+aws emr add-steps --cluster-id j-1HZAL6AHZ31US --steps file://./treasuredata.json --region ap-northeast-1
