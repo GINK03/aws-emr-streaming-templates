d9c5920aff7cc1724d7885b0f30cf4ec0b619614
Your Name <gim.kobayashi@gmail.com>
2017-11-17 09:11:34 +0900

    a

--- python3-treasuredata-timeseries/run.sh
+++ python3-treasuredata-timeseries/run.sh
@@ -1,1 +1,1 @@
-aws emr add-steps --cluster-id j-2NZYK3RPNQWE --steps file://./treasuredata.json --region ap-northeast-1
+aws emr add-steps --cluster-id j-2658Y59X09DB2 --steps file://./treasuredata.json --region ap-northeast-1
