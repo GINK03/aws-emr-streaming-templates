caa68b1645efabfac8d9760d0f90ec832bce078f
nardtree <gim.kobayashi@gmail.com>
2017-11-18 12:27:13 +0000

    a

--- python3-treasuredata-userdemographic-per-client/mapper.py
+++ python3-treasuredata-userdemographic-per-client/mapper.py
@@ -7,8 +7,17 @@ import urllib.parse
 import hashlib
 import codecs
 import pickle
+from datetime import datetime
 
 
+now = datetime.now()
+year_month1 = '%02d-%02d'%(now.year, now.month)
+if now.month != 1:
+  year_month2 = '%02d-%02d'%(now.year, now.month-1) 
+else:
+  year_month2 = '%02d-%02d'%(now.year-1, 12) 
+#print(year_month)
+#sys.exit()
 doi_key_keyword_freq = {}
 def main():
     #for line in sys.stdin:
@@ -28,6 +37,9 @@ def main():
       #print( obj )
 
       for time, data in obj.items():
+        # もし、処理の該当年月でないならスキップ
+        if year_month1 not in time and year_month2 not in time:
+          continue
         keyword = data[0]
         data_owner_id = data[-3]
         gender_age = data[-2]
--- python3-treasuredata-userdemographic-per-client/reducer.py
+++ python3-treasuredata-userdemographic-per-client/reducer.py
@@ -6,8 +6,21 @@ import pickle
 import codecs
 
 def main():
+  key_term_freq = {}
   for line in sys.stdin:
     line = line.strip()
-    print(line)
+    key, val = line.split('\t')
+    val = json.loads(val)
+    if key_term_freq.get(key) is None:
+      key_term_freq[key] = {}
+    for keyword, freq in val.items():
+      if key_term_freq[key].get(keyword) is None:
+        key_term_freq[key][keyword] = 0
+      key_term_freq[key][keyword] += freq
+
+    if len(key_term_freq) >= 2:
+      for key, term_freq in key_term_freq.items():
+        print(key, term_freq)
+      key_term_freq = {}
 if __name__ == "__main__":
     main()
